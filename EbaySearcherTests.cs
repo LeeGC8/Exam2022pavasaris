e93f2617fdcce8572b2dfb079823e88d71d68598
Liena Pupola <lienapupola@gmail.com>
2025-01-31 16:10:46 +0200

    Pievienots Ebay meklesanas funkcija un testi.

---
+++ EbaySearcherTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WindowsFormsApp1.Tests
+{
+    [TestFixture]
+    public class EbaySearcherTests
+    {
+        private IWebDriver driver;
+        private EbaySearcher searcher;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            searcher = new EbaySearcher();
+        }
+
+        [Test]
+        public void Test1_field()
+        {
+            searcher.OpenHomePage();
+            IWebElement searchBox = driver.FindElement(By.Id("gh-ac"));
+            Assert.IsNotNull(searchBox);
+        }cd
+
+        [Test]
+        public void Test2_search()
+        {
+            searcher.OpenHomePage();
+            IWebElement searchButton = driver.FindElement(By.Id("gh-btn"));
+            Assert.IsNotNull(searchButton);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            searcher.Close();
+            driver.Quit();
+        }
+    }
+}
---
+++ WindowsFormsApp1/EbaySearcher.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace WindowsFormsApp1
+{
+    public class EbaySearcher
+    {
+        private IWebDriver driver;
+
+        public EbaySearcher()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        public void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl("https://www.ebay.com");
+        }
+
+        public void Search(string searchTerm)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            IWebElement searchBox = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("gh-ac")));
+            searchBox.SendKeys(searchTerm);
+
+            IWebElement searchButton = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//input[@id='gh-btn']")));
+            searchButton.Click();
+        }
+
+        public string GetSearchResultUrl()
+        {
+            return driver.Url;
+        }
+
+        public void NavigateBack()
+        {
+            driver.Navigate().Back();
+        }
+
+        public void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
